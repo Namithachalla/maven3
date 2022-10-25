19e9c936ab16781876c77f038c67dc1eeb69a823
Namithachalla <115454257+Namithachalla@users.noreply.github.com>
2022-10-25 17:07:53 +0530

    Add files via upload

---
+++ src/test/java/bottle1 pkg/AppTest.java
@@ -0,0 +1,20 @@
+package bottle1 pkg;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
